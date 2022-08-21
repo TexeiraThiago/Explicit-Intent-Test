23efdc0a89116c3a8bea8a9c30d64f39e39df7c9
TexeiraThiago <thiagot@id.uff.br>
2022-08-21 20:14:29 -0300

    Finish activity2's layout

--- app/src/main/java/br/com/firstaplication/explictintent/Activity2.kt
+++ app/src/main/java/br/com/firstaplication/explictintent/Activity2.kt
@@ -2,10 +2,13 @@ package br.com.firstaplication.explictintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
     }
 }
