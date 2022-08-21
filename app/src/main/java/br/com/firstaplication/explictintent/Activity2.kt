635a60c20d9962ae02c7caee7494b378ce3006f8
TexeiraThiago <thiagot@id.uff.br>
2022-08-21 20:07:05 -0300

    Create empty activity2 and activity3

---
+++ app/src/main/java/br/com/firstaplication/explictintent/Activity2.kt
@@ -0,0 +1,11 @@
+package br.com.firstaplication.explictintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
---
+++ app/src/main/java/br/com/firstaplication/explictintent/Activity3.kt
@@ -0,0 +1,11 @@
+package br.com.firstaplication.explictintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_3)
+    }
+}
