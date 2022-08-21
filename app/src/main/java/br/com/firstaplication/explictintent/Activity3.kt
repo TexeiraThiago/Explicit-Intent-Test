1f0ab3ed9b401e5f51663020ab89958acb2ee789
TexeiraThiago <thiagot@id.uff.br>
2022-08-21 20:23:05 -0300

    Finish activity3's layout and correct textiview in MainActivity

--- app/src/main/java/br/com/firstaplication/explictintent/Activity3.kt
+++ app/src/main/java/br/com/firstaplication/explictintent/Activity3.kt
@@ -2,10 +2,19 @@ package br.com.firstaplication.explictintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Activity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_3)
+
+        val etSurname = findViewById<EditText>(R.id.etSurname)
+        val btnSurname = findViewById<Button>(R.id.btnSubmit)
+
+        btnSurname.setOnClickListener {
+
+        }
     }
 }
