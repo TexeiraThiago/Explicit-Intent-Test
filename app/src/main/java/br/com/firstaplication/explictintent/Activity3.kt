d7559318804a319ea27e8af958f3c68091de909a
TexeiraThiago <thiagot@id.uff.br>
2022-08-21 22:05:34 -0300

    Receive data from Activity3

--- app/src/main/java/br/com/firstaplication/explictintent/Activity3.kt
+++ app/src/main/java/br/com/firstaplication/explictintent/Activity3.kt
@@ -1,9 +1,11 @@
 package br.com.firstaplication.explictintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class Activity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +14,23 @@ class Activity3 : AppCompatActivity() {
 
         val etSurname = findViewById<EditText>(R.id.etSurname)
         val btnSurname = findViewById<Button>(R.id.btnSubmit)
+        val btnCancel = findViewById<Button>(R.id.btnCancel)
 
         btnSurname.setOnClickListener {
+            if (etSurname.text.toString().isEmpty()){
+                Toast.makeText(this, "Please type your Surname", Toast.LENGTH_SHORT).show()
+            }else {
+                val surName = etSurname.text.toString().trim()
+                val intent = Intent()
+                intent.putExtra("Surname", surName)
+                setResult(RESULT_OK,intent)
+                this.finish()
+            }
+        }
 
+        btnCancel.setOnClickListener {
+            setResult(RESULT_CANCELED)
+            this.finish()
         }
     }
 }
--- app/src/main/java/br/com/firstaplication/explictintent/MainActivity.kt
+++ app/src/main/java/br/com/firstaplication/explictintent/MainActivity.kt
@@ -3,32 +3,69 @@ package br.com.firstaplication.explictintent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import br.com.firstaplication.explictintent.Activity3 as Activity31
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val etName = findViewById<EditText>(R.id.etName)
-        val btnAcitivy2 = findViewById<Button>(R.id.btnActivity2)
-        val btnAcitivy3 = findViewById<Button>(R.id.btnActivity3)
+        val btnActivity2 = findViewById<Button>(R.id.btnActivity2)
+        val btnActivity3 = findViewById<Button>(R.id.btnActivity3)
         val tvResult = findViewById<TextView>(R.id.tvResult)
-        val ACTIVITY2 = br.com.firstaplication.explictintent.Activity2::class.java
-        val ACTIVITY3 = br.com.firstaplication.explictintent.Activity3::class.java
+        val ACTIVITY2 = Activity2::class.java
+        val ACTIVITY3 = Activity3::class.java
         val context = this
-        btnAcitivy2.setOnClickListener {
+
+        btnActivity2.setOnClickListener {
             if (etName.text.toString().isEmpty()) {
                 Toast.makeText(this, "Please type your name", Toast.LENGTH_SHORT).show()
             }else {
                 val text : String = etName.text.toString().trim()
-                val intent : Intent = Intent(context, ACTIVITY2)
+                val intent = Intent(context, ACTIVITY2)
                 intent.putExtra("Name", text)
                 startActivity(intent)
             }
+        }
+
+        val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result: ActivityResult ->
+
+            if (result.resultCode == RESULT_OK){
+                Log.d("Intent" , "result was: "+ result.data)
+                val intent = result.data
+
+                val text = intent?.getStringExtra("Surname") ?: "no data received"
+                tvResult.text = text
+
+//                if (intent != null) {
+//                    val text = intent.getStringExtra("Surname")
+//                    tvResult.setText(text)
+//                } else {
+//                    val textNull = "no text received"
+//                    tvResult.setText(textNull)
+//                }
+
+            }
+
+            if (result.resultCode == RESULT_CANCELED) {
+                val cancelText = "The Activity was closed and no data was send"
+                tvResult.text = cancelText
+            }
+
+            tvResult.visibility = View.VISIBLE
+        }
+
+        btnActivity3.setOnClickListener {
+            val intent = Intent(context, ACTIVITY3)
+            startForResult.launch(intent)
         }
 
     }
